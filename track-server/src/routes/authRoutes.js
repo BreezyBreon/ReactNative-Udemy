0162d325cc69b3fdad8078dc24345d566c4fa847
BreezyBreon <jsbreon@gmail.com>
2020-05-15 21:19:38 -0700

    JSON Web Token Creation

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post('/signup', async (req,res) => {
@@ -10,7 +11,8 @@ router.post('/signup', async (req,res) => {
     const user = new User({email, password});
     await user.save();
 
-    res.send("You made a post request to sign up");
+    const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY')
+    res.send({token});
     } catch (err) {
     return res.status(422).send(err.message);
     }
