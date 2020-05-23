91e6d3d00ee4a8d2e420cbd3c5e87200a0dc5989
BreezyBreon <jsbreon@gmail.com>
2020-05-22 22:57:32 -0700

    Through Section 192

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,9 +1,12 @@
 require('./models/User')
+require('./models/Track')
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+
 require('dotenv').config();
 const requireAuth = require('./middlewares/requireAuth');
 
@@ -12,6 +15,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
